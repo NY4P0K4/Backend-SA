5eddddc92016206d21609eee968955ec7506fe07
NY4P0K4 <lucas_debastiani@estudante.sesisenai.org.br>
2026-05-11 22:33:19 -0300

    Mudanças Finais
    - CRUD Terminado;
    - Front End Integrado com API;
    - Erros Corrigidos.
    
    esse front tá feio pra caralho.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const session = require('express-session');
 const livereload = require('livereload');
@@ -7,12 +9,12 @@ const app = express();
 const port = 3000
 
 const db = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'kanban',
-    password: 'senai',
-    port: 5432
-})
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
 
 db.query('SELECT NOW()', (err, res) => {
     if(err)
@@ -36,42 +38,62 @@ const dbMake = async() => {
             `CREATE TABLE IF NOT EXISTS transactions (
                 id SERIAL PRIMARY KEY,
                 userid INT DEFAULT 0,
-                value NUMERIC
+                categoria TEXT,
+                descricao TEXT,
+                valor NUMERIC,
+                data TEXT,
+                tipo TEXT
             )`);
     } catch(err) {
         return err.message;
     };
 };
 
-app.get('/transactions', async(req, res) => {
-    try {
-        const trs = await db.query('SELECT * FROM transactions');
-        return res.json({success: true, value: trs.rows});
-    } catch(err) {
-        return res.json({success: false, error: err.message});
-    };
-});
-
-app.get('/transactions/:id', async(req, res) => {
-    try {
-        const trs = await db.query('SELECT * FROM transactions WHERE id = $1', [req.params.id]);
-        return res.json({success: true, value: trs.rows});
-    } catch(err) {
-        return res.json({success: false, error: err.message});
-    };
-});
-
-app.post('/transactions', (req, res) => {
-    try {
-        return res.json({success: false, error: 'WIP'});
-    } catch(err) {
-        return res.json({success: false, error: err.message});
-    };
-});
-
 dbMake();
 
 app.use(express.static('public'));
 app.use(clr());
+app.use(express.json());
+
+app.get('/transactions', async(req, res) => {
+    try {
+        const trs = await db.query(`SELECT * FROM transactions`);
+        console.log(trs.rows);
+        return res.json(trs.rows);
+    } catch(err) {
+        return res.json({success: false, error: err.message});
+    };
+});
+
+app.put('/transactions/:id', async (req, res) => {
+    try {
+        const { categoria, descricao, valor, data, tipo } = req.body;
+        console.log(req.body)
+        const trs = await db.query('UPDATE transactions SET categoria = $1, descricao = $2, valor = $3, data = $4, tipo = $5 WHERE id = $6 RETURNING *', [categoria, descricao, valor, data, tipo, req.params.id]);
+        return res.json(trs.rows[0]);
+    } catch (err) {
+        return res.json({success: false, error: err.message});
+    }
+});
+
+app.post('/transactions', async (req, res) => {
+    try {
+        const {userid, categoria, descricao, valor, data, tipo} = req.body;
+        const trs = await db.query(`INSERT INTO transactions (userid, categoria, descricao, valor, data, tipo) VALUES ($1, $2, $3, $4, $5, $6)`, [userid ?? 0, categoria, descricao, valor, data, tipo]);
+        console.log(trs.rows);
+        return res.json(trs.rows[0]);
+    } catch(err) {
+        return res.json({success: false, error: err.message});
+    };
+});
+
+app.delete('/transactions/:id', async (req, res) => {
+    try {
+        await db.query('DELETE FROM transactions WHERE id = $1', [req.params.id]);
+        return res.sendStatus(204);
+    } catch(err) {
+        return res.json({success: false, error: err.message});
+    };
+});
 
 app.listen(port, () => {console.log(`Running on https://localhost:${port}`)});
